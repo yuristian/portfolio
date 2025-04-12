1ee686ffb7e81e7245c507db5c1d313a20b85d23
Yuristian <yuristian.ardianto@gmail.com>
2025-04-12 12:57:00 +0700

    add portfolio link

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -69,17 +69,13 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
-            {/*
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition">
-              <h3 className="text-xl font-bold mb-2">Jira Clone</h3>
+              <h3 className="text-xl font-bold mb-2">Snake</h3>
               <p className="text-gray-400 mb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-                tenetur accusamus reiciendis incidunt eligendi atque vitae
-                porro, cum quam repellendus quasi illum, tempora natus
-                reprehenderit sapiente maxime dolore et distinctio.
+                A Simple Snake Game using HTML and Javascript.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Next.js"].map((tech, key) => (
+                {["HTML", "Javascript"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
@@ -90,13 +86,14 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center my-4">
                 <a
-                  href="#"
+                  href="https://yuristian.github.io/js-snake/"
                   className="text-blue-400 hover:text-blue-300 transition-colors"
                 >
                   View Project
                 </a>
               </div>
             </div>
+            {/*
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition">
               <h3 className="text-xl font-bold mb-2">Jira Clone</h3>
               <p className="text-gray-400 mb-4">
